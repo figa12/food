eaa8cf2743a95aaf17e3f32cde556e87a85b87d2
bannednexus <jran10@student.aau.dk>
2014-02-24 14:37:42 +0100

    nyt projekt med navigation drawer

---
+++ recipe/app/src/main/java/aau/sw8/recipe/MainActivity.java
@@ -0,0 +1,170 @@
+package aau.sw8.recipe;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements SearchFragment.OnFragmentInteractionListener {
+
+    private DrawerLayout drawerLayout;
+    private ListView drawerListView;
+    private ActionBarDrawerToggle drawerToggle;
+
+    private CharSequence drawerTitle;
+    private CharSequence title;
+    private String[] planetTitles;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.title = this.drawerTitle = super.getTitle();
+        this.planetTitles = super.getResources().getStringArray(R.array.planets_array);
+        this.drawerLayout = (DrawerLayout) super.findViewById(R.id.drawer_layout);
+        this.drawerListView = (ListView) super.findViewById(R.id.left_drawer);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        this.drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // set up the drawer's list view with items and click listener
+        this.drawerListView.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, this.planetTitles));
+        this.drawerListView.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                MainActivity.this.selectItem(position);
+            }
+        });
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        super.getActionBar().setDisplayHomeAsUpEnabled(true);
+        super.getActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        this.drawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                this.drawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                MainActivity.super.getActionBar().setTitle(title);
+                MainActivity.super.invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                MainActivity.super.getActionBar().setTitle(drawerTitle);
+                MainActivity.super.invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        this.drawerLayout.setDrawerListener(this.drawerToggle);
+
+        if (savedInstanceState == null) {
+            this.selectItem(0);
+        }
+    }
+
+    private void selectItem(int position) {
+
+        Fragment fragment; // update the main content by replacing fragments
+        Bundle args = new Bundle();
+
+        // Insert cases for other fragments here
+        switch (position)
+        {
+            case 0:
+                fragment = new SearchFragment();
+                args.putInt(SearchFragment.ARG_POSITION, position);
+                fragment.setArguments(args);
+                break;
+
+            default:
+                fragment = new SearchFragment();
+                args.putInt(SearchFragment.ARG_POSITION, position);
+                fragment.setArguments(args);
+                break;
+        }
+
+        super.getFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        // update selected item and title, then close the drawer
+        this.drawerListView.setItemChecked(position, true);
+        this.setTitle(this.planetTitles[position]);
+        this.drawerLayout.closeDrawer(drawerListView);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        this.title = title;
+        super.getActionBar().setTitle(this.title);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        super.getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerListView);
+        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (this.drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle action buttons
+        switch(item.getItemId()) {
+            case R.id.action_websearch:
+                Toast.makeText(this, "Not implemented", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        this.drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        this.drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //TODO What does this do?!?!
+    }
+}
---
+++ recipe/app/src/main/java/aau/sw8/recipe/SearchFragment.java
@@ -0,0 +1,94 @@
+package aau.sw8.recipe;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SearchFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link SearchFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class SearchFragment extends Fragment {
+
+    public static final String ARG_POSITION = "position";
+
+    private OnFragmentInteractionListener interactionListener;
+
+    /**
+     * Use this factory method to create a new instance of this fragment
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SearchFragment newInstance(String param1, String param2) {
+        return new SearchFragment();
+    }
+    public SearchFragment() {
+        // Required empty public constructor (?)
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        int i = super.getArguments().getInt(SearchFragment.ARG_POSITION);
+
+        String planet = super.getResources().getStringArray(R.array.planets_array)[i];
+        ((TextView) rootView.findViewById(R.id.text)).setText(planet);
+        super.getActivity().setTitle(planet);
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (this.interactionListener != null) {
+            this.interactionListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            this.interactionListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        this.interactionListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
