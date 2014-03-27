5639bc72be42a1a8cf5bc5f3f4d3e6d322e3c499
Sam Olesen <samolesen@gmail.com>
2014-03-27 10:44:32 +0100

    Recipe notification og random

--- recipe/app/src/main/java/aau/sw8/recipe/SearchList.java
+++ recipe/app/src/main/java/aau/sw8/recipe/SearchList.java
@@ -3,6 +3,9 @@ package aau.sw8.recipe;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 /**
  * Created by Sam on 26/03/2014.
@@ -15,6 +18,52 @@ public class SearchList extends RecipeList {
 
     public SearchList(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+
+    @Override
+    protected View makeView(Recipe recipe) {
+        View recipeView = super.makeView(recipe);
+
+        // TODO proper data
+        TextView notification = (TextView) recipeView.findViewById(R.id.notification);
+
+        first = true;
+
+        moreNotifications(notification);
+        moreNotifications(notification);
+        moreNotifications(notification);
+        moreNotifications(notification);
+        moreNotifications(notification);
+
+
+
+        TextView rating = (TextView) recipeView.findViewById(R.id.rating);
+
+        for (int i = random.nextInt(5); i != 5; ++i) {
+            rating.append("★");
+        }
+
+        return recipeView;
+    }
+
+    private Random random = new Random();
+
+    private boolean first = true;
+
+    private void moreNotifications(TextView notificationView) {
+        String[] strings = new String[]{"Apples", "Lemons", "Eggs", "Flour", "Sugar"};
+
+        if (random.nextBoolean()) {
+            if (first)
+                first = false;
+            else
+                notificationView.append(", ");
+
+
+            notificationView.append(strings[random.nextInt(5)]);
+        }
+
     }
 
     @Override
