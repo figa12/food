90a8541c601b476ed6b0a84e1f0238d48f9f8b1b
BannedNexus <jerian@worldonline.dk>
2014-04-07 15:04:31 +0200

    foolproof ListLinearLayout

--- recipe/app/src/main/java/aau/sw8/recipe/ListLinearLayout.java
+++ recipe/app/src/main/java/aau/sw8/recipe/ListLinearLayout.java
@@ -17,10 +17,14 @@ import java.util.ArrayList;
 public abstract class ListLinearLayout<ListObject> extends LinearLayout {
 
     /** List of {@link ListObject} */
-    private ArrayList<ListObject> items = new ArrayList<ListObject>();
+    protected ArrayList<ListObject> items = new ArrayList<ListObject>();
+
+    /** If set to true, the next addView(View) is allowed */
+    private boolean allowedAddView = true;
 
     public ListLinearLayout(Context context) {
         super(context);
+        super.setOrientation(LinearLayout.VERTICAL);
     }
 
     public ListLinearLayout(Context context, AttributeSet attrs) {
@@ -51,36 +55,41 @@ public abstract class ListLinearLayout<ListObject> extends LinearLayout {
      * Add the specified {@link ListObject} at the bottom of this {@link aau.sw8.recipe.ListLinearLayout}.
      * @param listObject The object to add.
      */
-    public void addViewAtBottom(ListObject listObject) {
-        this.items.add(listObject);
-        super.addView(this.makeView(listObject));
+    public final void addViewAtBottom(ListObject listObject) {
+        this.addView(listObject);
     }
 
     /**
      * Add the specified {@link ListObject} to this {@link aau.sw8.recipe.ListLinearLayout}.
      * @param listObject
      */
-    public void addView(ListObject listObject) {
-        this.addViewAtBottom(listObject);
+    public final void addView(ListObject listObject) {
+        this.items.add(listObject);
+
+        this.allowedAddView = true;
+        super.addView(this.makeView(listObject));
     }
 
     /**
      * Add the specified {@link ListObject} at the top of this {@link aau.sw8.recipe.ListLinearLayout}.
      * @param listObject The object to add.
      */
-    public void addViewAtTop(ListObject listObject) {
+    public final void addViewAtTop(ListObject listObject) {
         //Add at index 0 in both
         this.items.add(0, listObject);
-        this.addView(this.makeView(listObject), 0);
+
+        this.allowedAddView = true;
+        super.addView(this.makeView(listObject), 0);
     }
 
     /**
      * Remove a {@link ListObject} at the specified index.
      * @param index The index in the {@link aau.sw8.recipe.ListLinearLayout} of the {@link ListObject}.
      */
-    public void removeView(int index) {
+    @Override
+    public final void removeViewAt(int index) {
         // Remove from both lists
-        this.removeViewAt(index);
+        super.removeViewAt(index);
         this.items.remove(index);
     }
 
@@ -88,8 +97,80 @@ public abstract class ListLinearLayout<ListObject> extends LinearLayout {
      * Searches the {@link aau.sw8.recipe.ListLinearLayout} for the {@link ListObject} and removes it.
      * @param listObject The {@link ListObject} to remove.
      */
-    public void removeView(ListObject listObject) {
+    public final void removeView(ListObject listObject) {
         // I think this will give an exception if the items doesn't exist. But you should not try to remove items that doesn't exist.
-        this.removeView(this.items.indexOf(listObject));
+        this.removeViewAt(this.items.indexOf(listObject));
+    }
+
+    @Override
+    @Deprecated
+    public final void addView(View view) {
+        // if the call to addView(View) came from within this class, it is allowed
+        if (this.allowedAddView) {
+            super.addView(view);
+            this.allowedAddView = false;
+        } else {
+            throw new RuntimeException("addView(View) is not allowed on a ListLinearLayout implementation");
+        }
+    }
+
+    @Override
+    @Deprecated
+    public final void addView(View view, int index) {
+        // if the call to addView(View) came from within this class, it is allowed
+        if (this.allowedAddView) {
+            super.addView(view, index);
+            this.allowedAddView = false;
+        } else {
+            throw new RuntimeException("addView(View) is not allowed on a ListLinearLayout implementation");
+        }
+    }
+
+    /**
+     * Please don't remove views by reference, remove with {@link ListObject} instead
+     * @param view to be removed
+     */
+    @Override
+    @Deprecated
+    public final void removeView(View view) {
+        int index = super.indexOfChild(view);
+        this.removeViewAt(index);
+        this.items.remove(index);
+    }
+
+    @Override
+    public final void removeAllViews() {
+        super.removeAllViews();
+        this.items.clear();
+    }
+
+    @Override
+    public final void removeAllViewsInLayout() {
+        this.removeAllViews();
+    }
+
+    @Override
+    public final void removeViews(int start, int count) {
+        super.removeViews(start, count);
+        for (int i = 0; i < count; i++) {
+            // continuously remove at the start index, because the indices changes
+            this.items.remove(start);
+        }
+    }
+
+    /**
+     * Please don't remove views by reference, remove with {@link ListObject} instead
+     * @param view to be removed
+     */
+    @SuppressWarnings("deprecation")
+    @Override
+    @Deprecated
+    public final void removeViewInLayout(View view) {
+        this.removeView(view);
+    }
+
+    @Override
+    public final void removeViewsInLayout(int start, int count) {
+        this.removeViews(start, count);
     }
 }
