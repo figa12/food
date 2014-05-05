86bfb19b9781cfdde9ad35fbc029fb8e30ad0a64
Jacob Wortmann <jacob@wortmann.dk>
2014-05-05 10:52:42 +0200

    revork af servermessage

--- recipe/app/src/main/java/aau/sw8/data/FavouriteListCom.java
+++ recipe/app/src/main/java/aau/sw8/data/FavouriteListCom.java
@@ -29,7 +29,7 @@ public class FavouriteListCom extends ServerComTask<ArrayList<IntermediateRecipe
         apiPath += "&hash=" + hash;
         apiPath += "&limit=" + String.valueOf(limit);
         apiPath += "&offset=" + String.valueOf(offset);
-        apiPath += "&lang=" + lang;
+        apiPath += "&lang=" +  lang;
 
         return apiPath;
     }
